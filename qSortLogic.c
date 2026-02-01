f2cd0719affaf59c39ea7521e15af746f6afd871
durvesh <thes86088@gmail.com>
2026-02-01 23:35:34 +0530

    rectify condition for swapping of "left" and "right" pointers/observers.
    stuck at infinite recursion

--- qSortLogic.c
+++ qSortLogic.c
@@ -1,4 +1,6 @@
 
+#include <stdio.h>
+
 int swap( int arr[], int i, int j )
 {
   int t = arr[i];
@@ -6,9 +8,18 @@ int swap( int arr[], int i, int j )
   arr[j] = t;
 }
 
+void printArr( int arr[], int len )
+{
+    for( int i=0; i<len; i++ )
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
 int partition( int arr[], int low, int high )
 {
-  int left=low, right = high, pivot = 0;
+  int left=low, right = high, pivot = low;
   while( left < right )
     {
       while( arr[left]<=arr[pivot] )
@@ -20,7 +31,7 @@ int partition( int arr[], int low, int high )
         {
           right -- ;
         }
-      if( arr[left]>arr[right] )
+      if( arr[left]<arr[right] )
       {
         swap( arr, left, right);
       }
@@ -37,7 +48,17 @@ void quickSort( int arr[], int low, int high )
     int split = partition( arr, low, high );
 
     quickSort( arr, low, split-1 );
-    quickSort( arr, split+1, high );
+    quickSort( arr, split+1, high ); 
   }
+  return;
+  
 }
 
+int main()
+{
+    int arr[] = { 312,3,34,34,454,5654,76,77,87,867,8,6,6,35,34,33,13,35,57,68,898,96,5523,413 };
+    int len = (sizeof(arr))/(sizeof(int)) ;
+    printArr( arr, len );
+    quickSort(arr, 0, len-1);
+    printArr(arr, len);
+}
